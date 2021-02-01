e2b13f2c97f92887cd152c09654559149b40fe7a
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-01 17:11:16 +0700

    Изменены (закомментированы) свойства в файл Post.kt

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -22,10 +22,10 @@ data class Post(
     val likes: Likes,
     val reposts: Reposts,
     val views: Views,
-    val donut: Donut,
-    val postSource: PostSource,
-    var geo: Geo,
-    var copyHistory: Array<Any>
+    val donut: Donut
+//    val postSource: PostSource,
+//    var geo: Geo,
+//    var copyHistory: Array<Any> // TODO
 )
 
 class Comments(
@@ -71,30 +71,30 @@ class Placeholder(
     val placeholder: Boolean
 )
 
-class PostSource(
-    val type: String,
-    val platform: String,
-    val data: String,
-    val url: String
-)
-
-class Geo(
-    val type: String,
-    var coordinates: String,
-    val place: Place
-)
-
-class Place(
-    val id: Int,
-    val title: String,
-    val latitude: Int,
-    val longitude: Int,
-    val created: Int,
-    val icon: String,
-    val checkins: Int,
-    val updated: Int,
-    val type: Int,
-    val country: Int,
-    val city: Int,
-    val address: String
-)
+//class PostSource(
+//    val type: String,
+//    val platform: String,
+//    val data: String,
+//    val url: String
+//)
+//
+//class Geo(
+//    val type: String,
+//    var coordinates: String,
+//    val place: Place
+//)
+//
+//class Place(
+//    val id: Int,
+//    val title: String,
+//    val latitude: Int,
+//    val longitude: Int,
+//    val created: Int,
+//    val icon: String,
+//    val checkins: Int,
+//    val updated: Int,
+//    val type: Int,
+//    val country: Int,
+//    val city: Int,
+//    val address: String
+//)
